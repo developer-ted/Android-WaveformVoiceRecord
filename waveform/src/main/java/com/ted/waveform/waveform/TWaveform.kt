86a8768a6af7b2e8e81472d984501ad87ae0f17e
rlawldhsk <rlawldhsk@gmail.com>
2019-05-06 17:38:16 +0900

    add Waveform module

---
+++ waveform/src/main/java/com/ted/waveform/waveform/TWaveform.kt
@@ -0,0 +1,12 @@
+package com.ted.waveform.waveform
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+
+class TWaveform(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    
+
+}
